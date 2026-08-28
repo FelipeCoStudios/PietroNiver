d0b5ac3f6eec61d0f6482a1b07f4e5aa9597d307
FelipeCoStudios <felipe.jerez@cpviva.g12.br>
2026-08-28 18:36:48 -0300

    Loader for Resenha Fut 9C edition

--- game.js
+++ game.js
@@ -1,6 +1,16 @@
 (() => {
-'use strict';
-
-// TITLE UPDATED - FULL FILE FOLLOWS VIA LOCAL
-console.error('incomplete');
+const parts = [];
+function loadNext(i) {
+  if (i >= 2) {
+    const s = atob(parts.join(''));
+    (0, eval)(s);
+    return;
+  }
+  const sc = document.createElement('script');
+  sc.src = 'game-data-' + i + '.js';
+  sc.onload = () => loadNext(i+1);
+  document.head.appendChild(sc);
+}
+window.__pushGameB64 = (chunk) => parts.push(chunk);
+loadNext(0);
 })();
