80d6ccdf372246699803ba2a1240cfd65aa57734
FelipeCoStudios <felipe.jerez@cpviva.g12.br>
2026-08-28 18:38:47 -0300

    Full Resenha Fut 9C edition game (chunked load)

--- game.js
+++ game.js
@@ -1,13 +1,20 @@
 (() => {
 async function run() {
-  const bin = Uint8Array.from(atob("PLACEHOLDER_USE_FILE"), c => c.charCodeAt(0));
+  const b64 = (window.__G || []).join('');
+  const bin = Uint8Array.from(atob(b64), c => c.charCodeAt(0));
   const ds = new DecompressionStream("gzip");
   const stream = new Blob([bin]).stream().pipeThrough(ds);
   const s = await new Response(stream).text();
   (0, eval)(s);
 }
-run().catch(e => {
+const load = (i) => new Promise((res, rej) => {
+  const sc = document.createElement('script');
+  sc.src = 'gchunk' + i + '.js';
+  sc.onload = res; sc.onerror = rej;
+  document.head.appendChild(sc);
+});
+Promise.all([0,1,2].map(load)).then(run).catch(e => {
   console.error(e);
-  document.body.innerHTML = "<h1 style=\"color:#FFD700;font-family:Arial;text-align:center;margin-top:40vh\">Resenha Fut 9C edition</h1>";
+  document.body.innerHTML = '<h1 style="color:#FFD700;font-family:Arial;text-align:center;margin-top:40vh">Resenha Fut 9C edition</h1>';
 });
 })();
